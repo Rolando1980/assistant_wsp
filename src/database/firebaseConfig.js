21a04c35b83f867d69d92de391ce5a70e01714b3
Rolando1980 <martinez.angeles@outlook.com>
2025-02-28 11:08:03 -0500

    Eliminando firebase-credentials.json del historial

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,8 +1,12 @@
 import admin from "firebase-admin";
 import fs from "fs";
+import path from "path";
+
+// Obtener la ruta absoluta del archivo de credenciales
+const credentialsPath = path.resolve("src/database/firebase-credentials.json");
 
 // Leer las credenciales de Firebase desde el archivo JSON
-const serviceAccount = JSON.parse(fs.readFileSync("C:/Users/Comercial-01/Documents/Rolando/Desarrollos/Ememsa_bot/src/database/firebase-credentials.json", "utf8"));
+const serviceAccount = JSON.parse(fs.readFileSync(credentialsPath, "utf8"));
 
 // Validación básica de credenciales
 if (!serviceAccount || !serviceAccount.project_id) {
@@ -12,7 +16,7 @@ if (!serviceAccount || !serviceAccount.project_id) {
 // Inicializar Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ememsa-bot-default-rtdb.firebaseio.com/", 
+  databaseURL: "https://ememsa-bot-default-rtdb.firebaseio.com/",
 });
 
 // Exportar la base de datos para usarla en otros archivos
